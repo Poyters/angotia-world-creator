7fdc9e7b06440d9af41e7ae8994bfeb5c89eaa24
Rafał Kostecki <edward-12@o2.pl>
2019-03-29 13:51:50 +0100

    Rewrite dragElement function

--- src/assets/scripts/dragElement.ts
+++ src/assets/scripts/dragElement.ts
@@ -1,37 +1,34 @@
 const dragElement = (element:any) => {
-    let positions:Array<number> = [0, 0, 0, 0];
-    
-    element.onmousedown = dragMouseDown;
+  let positions:Array<number> = [0, 0, 0, 0];
   
-    function dragMouseDown(e) {
-      e = e || window.event;
-      e.preventDefault();
-      // get the mouse cursor position at startup:
-      positions[2] = e.clientX;
-      positions[3] = e.clientY;
-      document.onmouseup = closeDragElement;
-      // call a function whenever the cursor moves:
-      document.onmousemove = elementDrag;
-    }
-  
-    function elementDrag(e) {
-      e = e || window.event;
-      e.preventDefault();
-      // calculate the new cursor position:
-      positions[0] = positions[2] - e.clientX;
-      positions[1] = positions[3] - e.clientY;
-      positions[2] = e.clientX;
-      positions[3] = e.clientY;
-      // set the element's new position:
-      element.style.top = (element.offsetTop - positions[1]) + "px";
-      element.style.left = (element.offsetLeft - positions[0]) + "px";
-    }
-  
-    function closeDragElement() {
-      // stop moving when mouse button is released:
-      document.onmouseup = null;
-      document.onmousemove = null;
-    }
+  const dragMouseDown = (e: MouseEvent): void => {
+    e.preventDefault();
+
+    positions[2] = e.clientX;
+    positions[3] = e.clientY;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
   }
+
+  const elementDrag = (e: MouseEvent): void => {
+    e.preventDefault();
+
+    positions[0] = positions[2] - e.clientX;
+    positions[1] = positions[3] - e.clientY;
+    positions[2] = e.clientX;
+    positions[3] = e.clientY;
+
+    element.style.top = (element.offsetTop - positions[1]) + "px";
+    element.style.left = (element.offsetLeft - positions[0]) + "px";
+  }
+
+  const closeDragElement = (): void => {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+
+  element.onmousedown = dragMouseDown;
+}
 
 export default dragElement;
