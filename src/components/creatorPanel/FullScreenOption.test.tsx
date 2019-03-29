2edd8f42e3a897665c1186996d52b150bdd574a7
Rafał Kostecki <edward-12@o2.pl>
2019-03-29 15:21:16 +0100

    Add enzyme and write FullScreenOption test className changing

--- src/components/creatorPanel/FullScreenOption.test.tsx
+++ src/components/creatorPanel/FullScreenOption.test.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure } from 'enzyme';
+
+//Import components
 import FullScreenOption from './FullScreenOption';
 
+
+configure({adapter: new Adapter()});
 
 it('FullScreenOption component renders without crashing', () => {
   const div: HTMLElement = document.createElement('div');
   ReactDOM.render(<FullScreenOption />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+
+test('FullScreenOption change class name to on', () => {
+  const fullScreenOption = shallow(<FullScreenOption/>);
+
+  expect(fullScreenOption.hasClass('option--off')).toEqual(true);
+  fullScreenOption.simulate('click');
+  expect(fullScreenOption.hasClass('option--on')).toEqual(true);
 });
