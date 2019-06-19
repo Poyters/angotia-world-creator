179062fbc4a1bfc698ed06c7841cc5c5630faaf0
Rafał Kostecki <edward-12@o2.pl>
2019-06-19 13:35:00 +0200

    Add MapSizeInput root test

---
+++ src/components/partials/entryPanel/MapSizeInput.test.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure } from 'enzyme';
+
+//Import component
+import MapSizeInput from './MapSizeInput';
+
+
+configure({adapter: new Adapter()});
+
+it('MapSizeInput component renders without crashing', (): void => {
+  const div: HTMLElement = document.createElement('div');
+  ReactDOM.render(<MapSizeInput id="testInput" currValue={10} changeValue={() => {}} />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
