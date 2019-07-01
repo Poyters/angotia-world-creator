5b18feab5aa804d0ccc0ee5b7fef20cb6bad619a
Rafał Kostecki <edward-12@o2.pl>
2019-07-01 15:54:11 +0200

    Rewrite redux to redux hooks for NetOption component

--- src/components/partials/creatorPanel/NetOption.tsx
+++ src/components/partials/creatorPanel/NetOption.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 //Import scripts
 import { drawMapNet } from '../../../assets/scripts/drawNetMap';
@@ -9,19 +9,15 @@ import { setActionNote } from '../../../assets/scripts/notifications';
 //Import actions
 import { setMapNets } from '../../../redux/actions/mapActions';
 
-//Import interfaces
-import { IMapSize } from '../../../assets/interfaces/mapInterfaces';
-
 
 interface INetOption {
-  viewTypeQuantity: number,
-  mapSize: IMapSize,
-  setMapNets: Function
+  viewTypeQuantity: number
 }
 
 
-const NetOption: React.FC<INetOption> = ({ viewTypeQuantity, setMapNets }) => {
+const NetOption: React.FC<INetOption> = ({ viewTypeQuantity }) => {
   const [optionViewType, setOptionViewType] = useState<number>(0);
+  const dispatch = useDispatch();
 
   const changeViewType = (): void => {
     if (optionViewType === viewTypeQuantity) setOptionViewType(0);
@@ -35,21 +31,21 @@ const NetOption: React.FC<INetOption> = ({ viewTypeQuantity, setMapNets }) => {
       case 0: //all nets
         drawMapNet(ctx, 1);
         drawMapNet(ctx, 0);
-        setMapNets({field: true, square: true})
+        dispatch(setMapNets({field: true, square: true}));
         setActionNote("Square and field nets are visible now");
       break;
       case 1: //field net
         drawMapNet(ctx, 0);
-        setMapNets({field: true, square: false})
+        dispatch(setMapNets({field: true, square: false}));
         setActionNote("Only field nets are visible");
       break;
       case 2: //square net;
         drawMapNet(ctx, 1);
-        setMapNets({field: false, square: true})
+        dispatch(setMapNets({field: false, square: true}));
         setActionNote("Only square nets are visible");
       break;
       case 3:
-        setMapNets({field: false, square: false})
+        dispatch(setMapNets({field: false, square: false}));
         setActionNote("Disable all nets");
         return;
     }
@@ -71,11 +67,4 @@ const NetOption: React.FC<INetOption> = ({ viewTypeQuantity, setMapNets }) => {
 }
 
 
-const mapDispatchToProps = dispatch => {
-  return {
-    setMapNets: netStatus => {dispatch(setMapNets(netStatus))}
-  }
-}
-
-
-export default connect(null, mapDispatchToProps)(NetOption);
+export default NetOption;
