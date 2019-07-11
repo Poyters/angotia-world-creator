cb66db3c2308ca1007bf84856789911133547ecd
Rafał Kostecki <edward-12@o2.pl>
2019-07-11 15:42:51 +0200

    Create unblock squares option

--- src/assets/scripts/blockSquares.ts
+++ src/assets/scripts/blockSquares.ts
@@ -14,11 +14,16 @@ import { changeMapSelectMatrix } from '../../redux/actions/mapActions';
 import { changeMapBlockMatrix } from '../../redux/actions/mapActions';
 
 
+let pressedKey: number = -1;
+
+document.addEventListener('keydown', event => pressedKey = event.keyCode);
+
 export const setBlockSquares = () => {
   const storeData = store.getState();
   const selectMatrix: any[] = [...storeData.map.select.matrix];
   const blockMatrix: any[] = [...storeData.map.blockMatrix];
-  const newBlockMatrix = updateMatrixByTheOther(blockMatrix, selectMatrix);
+  const typeOfAction = pressedKey === creatorConfig.secondOptionKeyCode ? 0 : 1; //secondOptionKeyCode determines second type of used action, eg at this example, you can set block square, but when you press key wich has the same code as secondOptionKeyCode, you unblock selected fields
+  const newBlockMatrix = updateMatrixByTheOther(blockMatrix, selectMatrix, typeOfAction);
 
   store.dispatch(changeMapBlockMatrix(newBlockMatrix));
   clearCanvas("mapSelectCanvas", changeMapSelectMatrix);
@@ -26,10 +31,12 @@ export const setBlockSquares = () => {
   emptyMapCanvasCtx('mapBlockCanvas');
   colorBasedOnMatrix(newBlockMatrix, 'mapBlockCanvas', creatorConfig.blockSquareColor, 'barrier');
   setActionNote('Selected fields had been blocked');
+
+  setTimeout(() => pressedKey = -1, 500) //clear pressedKey. Duration is necessary due to pressing key for a while after running setBlockSquares
 }
 
 
-const updateMatrixByTheOther = (rootMatrix: any[], upgradeMatrix: any[]): any[] => {
+const updateMatrixByTheOther = (rootMatrix: any[], upgradeMatrix: any[], setValue: number): any[] => {
   upgradeMatrix.forEach((yAxis: Array<number>, y:number) => {
     yAxis.forEach((field: number, x: number) => {
       const squareMatrix: Array<number> = [
@@ -44,9 +51,7 @@ const updateMatrixByTheOther = (rootMatrix: any[], upgradeMatrix: any[]): any[] 
           const fieldArray = squareIndex >= 2 ? 1 : 0;
           const squarePos = fieldArray === 0 ? squareIndex : squareIndex - 2;
 
-
-          console.log('upgradeMatrix', upgradeMatrix[y][x][fieldArray][squarePos]);
-          rootMatrix[y][x][fieldArray][squarePos] = 1; //add new values to the rootMatrix
+          rootMatrix[y][x][fieldArray][squarePos] = setValue; //add new values to the rootMatrix
         }
       });
 
