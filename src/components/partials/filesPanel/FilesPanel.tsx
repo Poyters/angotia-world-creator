e4d7a9283d84b666dec14d5a8e6350b3f5a683b4
Rafał Kostecki <edward-12@o2.pl>
2019-07-11 16:24:21 +0200

    Create abillity to hide/show filesPanel

--- src/components/partials/filesPanel/FilesPanel.tsx
+++ src/components/partials/filesPanel/FilesPanel.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 
 
 const FilesPanel = () => {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
+    const filesPanelStyles = {
+        right: isOpen ? "0" : "-300px"
+    }
+
     return (
-        <aside className="filesPanelWrapper">
-            <div className="filesPanel">
-                FilesPanel component
-            </div>
-        </aside>
+        <Fragment>
+            { !isOpen ? (
+                <div className="filesPanelSwitch t-paragraph4Normal" onClick={() => setIsOpen(true)}> Open files panel</div>
+            ) : null}
+            <aside className="filesPanelWrapper" style={filesPanelStyles}>
+                <div className="filesPanel">
+                    FilesPanel component
+
+                    <div className="filesPanel__switch t-paragraph4Normal" onClick={() => setIsOpen(false)}>Hide files panel</div>
+                </div>
+            </aside>
+        </Fragment>
     )
 }
 
