ff79fb1678de88752cdbfdb04ae30244fee8c71c
Rafał Kostecki <edward-12@o2.pl>
2019-08-06 17:05:43 +0200

    Rewrite markSquare

--- src/assets/scripts/markSquare.ts
+++ src/assets/scripts/markSquare.ts
@@ -12,25 +12,24 @@ import creatorConfig from '../configs/creatorConfig.json';
 
 //Import actions
 import { changeMapSelectMatrix } from '../../redux/actions/mapActions';
-import { changeMapPassageMatrix } from '../../redux/actions/mapActions';
 
 
 let pressedKey: number = -1;
 
 document.addEventListener('keydown', event => pressedKey = event.keyCode);
 
-export const markSquare = (sourceMatrix: any[], sourceMatrixCanvas: string, note: string, fillColor: string, fillStyle?: string) => {
+export const markSquare = (sourceMatrix: any[], sourceMatrixCanvas: string, changeMatrixMethod: Function, note: string, fillColor: string, fillStyle?: string) => {
   const storeData = store.getState();
   const selectMatrix: any[] = deepCopy(storeData.map.select.matrix);
   const sourceMatrixCopy: any[] = deepCopy(sourceMatrix);
   const typeOfAction: number = pressedKey === creatorConfig.secondOptionKeyCode ? 0 : 1; //secondOptionKeyCode determines second type of used action, eg at this example, you can add passage, but when you press key wich has the same code as secondOptionKeyCode, you delete selected passage
-  const newPassageMatrix: any[] = deepCopy(updateMatrixByTheOther(sourceMatrixCopy, selectMatrix, typeOfAction));
-  
-  store.dispatch(changeMapPassageMatrix(newPassageMatrix));
+  const newMatrix: any[] = deepCopy(updateMatrixByTheOther(sourceMatrixCopy, selectMatrix, typeOfAction));
+
+  store.dispatch(changeMatrixMethod(newMatrix));
   clearCanvas("mapSelectCanvas", changeMapSelectMatrix);
 
   emptyMapCanvasCtx(sourceMatrixCanvas);
-  colorBasedOnMatrix(newPassageMatrix, sourceMatrixCanvas, fillColor, fillStyle);
+  colorBasedOnMatrix(newMatrix, sourceMatrixCanvas, fillColor, fillStyle);
   setActionNote(note);
 
   setTimeout(() => pressedKey = -1, 500) //clear pressedKey. Duration is necessary due to pressing key for a while after running setBlockSquares
--- src/components/partials/creatorPanel/PassageOption.tsx
+++ src/components/partials/creatorPanel/PassageOption.tsx
@@ -4,12 +4,15 @@ import { useSelector } from 'react-redux';
 //Import scripts
 import { markSquare } from '../../../assets/scripts/markSquare';
 
+//Import actions
+import { changeMapPassageMatrix } from '../../../redux/actions/mapActions';
+
 
 const PassageOption: React.FC = () => {
     const passageMatrix = useSelector(state => state.map.passage.matrix);
 
     return (
-        <div className="passageOption" onClick={() => markSquare(passageMatrix, 'mapPassageCanvas', 'Added passage', '#fff')}></div>
+        <div className="passageOption" onClick={() => markSquare(passageMatrix, 'mapPassageCanvas', changeMapPassageMatrix, 'Passage added', '#fff', '')}></div>
     )
 }
 
