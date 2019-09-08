8920816b6a59fa8d357ffa228f31dafdaa2a4c88
RafalKostecki <edward-12@o2.pl>
2019-09-08 17:15:52 +0200

    Add abillity to save board - development mode

--- src/components/partials/creatorPanel/SaveOption.tsx
+++ src/components/partials/creatorPanel/SaveOption.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+//Import scripts
+import { saveFile } from '../../../assets/scripts/saveFile';
 
 
 const SaveOption: React.FC = () => {
+  const mapData = useSelector(state => state.map);
+
+  const saveMap = () => {
+    saveFile(JSON.stringify(mapData), 'boardName.json', 'text/json');
+  }
 
   return (
-    <div role="button" className="option option--textOption option--smallerMargin" data-title="save board on your computer">
+    <div role="button" className="option option--textOption option--smallerMargin" data-title="save board on your computer" onClick={() => saveMap()}>
       <span>Save</span>
     </div>
   );
