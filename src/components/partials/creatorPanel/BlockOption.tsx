551da1c0ae97b7e2d5f73dc034895ab106b89e23
RafalKostecki <edward-12@o2.pl>
2019-09-08 03:28:53 +0200

    Disable BlockOption when any fields are selected

--- src/components/partials/creatorPanel/BlockOption.tsx
+++ src/components/partials/creatorPanel/BlockOption.tsx
@@ -3,6 +3,8 @@ import { useSelector } from 'react-redux';
 
 //Import scripts
 import { markSquare } from '../../../assets/scripts/markSquare';
+import { isEmptyMatrix } from '../../../assets/scripts/isEmptyMatrix';
+import { setActionNote } from '../../../assets/scripts/notifications';
 
 //Import configs
 import creatorConfig from '../../../assets/configs/creatorConfig.json';
@@ -14,9 +16,19 @@ import { changeMapBlockMatrix } from '../../../redux/actions/mapActions';
 const BlockOption = () => {
 	const blockMatrix = useSelector(state => state.map.blockMatrix);
 	const fillColor = creatorConfig.blockSquareColor;
+	const selectMatrix = useSelector(state => state.map.select.matrix);
+
+	const blockHandler = (): void => {
+		if (isEmptyMatrix(selectMatrix)) {
+            setActionNote('Need to select fields', 'warning');
+            return;
+        }
+		
+		markSquare(blockMatrix, 'mapBlockCanvas', changeMapBlockMatrix, 'Selected fields have been blocked', fillColor, 'barrier');
+	}
 
 	return (
-		<div role="button" className="option option--block" onClick={() => markSquare(blockMatrix, 'mapBlockCanvas', changeMapBlockMatrix, 'Selected fields have been blocked', fillColor, 'barrier')} data-title="add/delete block squares">
+		<div role="button" className="option option--block" onClick={() => blockHandler()} data-title="add/delete block squares">
 			<div className="g-exitBtn"></div>
 		</div>
 	)
