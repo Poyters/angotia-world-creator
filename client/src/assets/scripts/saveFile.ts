357185084df97923e3c9e91445b7184e7fea39eb
Rafał Kostecki <edward-12@o2.pl>
2019-10-02 23:16:41 +0200

    Add save option

---
+++ client/src/assets/scripts/saveFile.ts
@@ -0,0 +1,18 @@
+export const saveFile = (data: any, filename: string, type: string): void => {
+    const file = new Blob([data], {type: type});
+    
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        const a: HTMLAnchorElement = document.createElement("a");
+        const url: string = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(() => {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);  
+        }, 0); 
+    }
+}
