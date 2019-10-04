8c23090b6a1dd4390d2a9f3981c1ae77dd6ab5b7
Rafał Kostecki <edward-12@o2.pl>
2019-10-04 12:20:22 +0200

    Add select field script test

--- client/src/assets/scripts/selectFields.test.ts
+++ client/src/assets/scripts/selectFields.test.ts
@@ -17,12 +17,40 @@ describe("selectCanvasSquare script", () => {
 		y: 4
 	}
 
-	it("Check select square)", () => {
+	it("Check select square", () => {
 		selectCanvasSquare(exampleMatrix, examplePoint);
-
 		const square = exampleMatrix[2][1][0][0];
 
     expect(square).toEqual(1);
+	});
+
+});
+
+describe("selectCanvasField script", () => {
+	const exampleMatrix: any[] = [
+		[
+			[[0, 0], [0, 0]], [[0, 0], [0, 0]], [[0, 0], [0, 0]]
+		],
+		[[[0, 0], [0, 0]], [[0, 0], [0, 0]], [[0, 0], [0, 0]]],
+		[[[0, 0], [0, 0]], [[0, 0], [0, 0]], [[0, 0], [0, 0]]],
+		[[[0, 0], [0, 0]], [[0, 0], [0, 0]], [[0, 0], [0, 0]]],
+		[[[0, 0], [0, 0]], [[0, 0], [0, 0]], [[0, 0], [0, 0]]]
+	];
+
+	const examplePoint: IPoint = {
+		x: 2,
+		y: 4
+	}
+
+	it("Check select filed", () => {
+		selectCanvasField(exampleMatrix, examplePoint);
+		const field = exampleMatrix[4][2];
+		const selectedField = [
+			[1, 1],
+			[1, 1]
+		];
+		
+    expect(field).toEqual(selectedField);
 	});
 
 });
