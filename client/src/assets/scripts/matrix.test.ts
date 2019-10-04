d8124e7acef4c54e85ec4c11491f21cfaa93e2c2
Rafał Kostecki <edward-12@o2.pl>
2019-10-04 20:39:35 +0200

    Add matrixToIds script tests

--- client/src/assets/scripts/matrix.test.ts
+++ client/src/assets/scripts/matrix.test.ts
@@ -1,5 +1,5 @@
 //Import component
-import { updateMatrixByTheOther } from './matrix';
+import { updateMatrixByTheOther, matrixToIds } from './matrix';
 
 
 describe("updateMatrixByTheOther script", () => {
@@ -29,6 +29,28 @@ describe("updateMatrixByTheOther script", () => {
 	it("Update not empty matrix by the other", () => {
 		const updatedMatrix = updateMatrixByTheOther(notEmptyRootMatrix, updateMatrix, 1);
     expect(updatedMatrix[0][0]).toEqual([[1, 1], [1, 1]]);
+	});
+	
+});
+
+
+describe("matrixToIds script", () => {
+	const matrix: any[] = [
+		[[[1, 0], [0, 0]], [[0, 0], [0, 0]], [[0, 0], [0, 0]]],
+		[[[0, 0], [0, 0]], [[0, 1], [0, 0]], [[0, 0], [0, 0]]],
+		[[[0, 0], [0, 0]], [[0, 0], [0, 0]], [[1, 0], [0, 0]]]
+  ];
+    
+	it("Update empty matrix by the other", () => {
+		const squareIds = matrixToIds(matrix);
+		const expectedOutput = [
+			{id: "0.0, 0.0", x: 0, xDelta: 0, y: 0, yDelta: 0}, 
+			{id: "1.1, 1.0", x: 1, xDelta: 1, y: 1, yDelta: 0}, 
+			{id: "2.0, 2.0", x: 2, xDelta: 0, y: 2, yDelta: 0}
+		];
+
+		expect(squareIds).toEqual(expectedOutput);
+		expect(squareIds.length).toEqual(3);
 	});
 	
 });
