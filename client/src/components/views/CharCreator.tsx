e7f259637bd0753a8c652cd9e8b0a5639fb5736d
Rafał Kostecki <edward-12@o2.pl>
2019-11-21 21:47:23 +0100

    Create CharCreatorForm component

--- client/src/components/views/CharCreator.tsx
+++ client/src/components/views/CharCreator.tsx
@@ -6,12 +6,14 @@ import CreditsFooter from '../partials/CreditsFooter';
 import Notifications from '../partials/Notifications';
 import VersionMark from '../partials/VersionMark';
 import ReportIssue from '../partials/ReportIssue';
+import CharCreatorForm from '../partials/charCreatorForm/CharCreatorForm';
 
 
 const CharCreator: React.FC = () => {
   return (
     <article className="creatorWrapper">
       <CharPanel />
+      <CharCreatorForm />
       <CreditsFooter />
       <Notifications />
       <VersionMark />
