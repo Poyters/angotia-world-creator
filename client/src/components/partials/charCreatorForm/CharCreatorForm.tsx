6e919ef76c5fe0e334b95a9b7ee577c7c26e5d63
Rafał Kostecki <edward-12@o2.pl>
2019-11-22 19:44:55 +0100

    Create __graphice and __baseInfo for charForm

--- client/src/components/partials/charCreatorForm/CharCreatorForm.tsx
+++ client/src/components/partials/charCreatorForm/CharCreatorForm.tsx
@@ -11,23 +11,14 @@ const CreatorForm: React.FC = () => {
           Character
         </header>
         <div className="charCreatorForm">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec nibh a velit ultricies finibus id ut risus. Aenean at fermentum enim, at efficitur arcu. Mauris ultricies massa ut quam pulvinar elementum. Nunc ut dolor in mauris egestas dictum. Aenean nisl ante, mattis non metus sodales, tempor pretium ante. Fusce congue semper lorem vel gravida. Aliquam laoreet sapien vitae sem aliquam, suscipit bibendum mi venenatis. Ut eu elementum purus, non convallis lacus. Mauris maximus sed dolor in convallis. Vestibulum at nisi odio. Nulla vel ligula consectetur, dictum leo at, fringilla risus. Aliquam dui enim, vehicula volutpat dictum eget, eleifend a libero. In accumsan purus vel euismod efficitur. Morbi placerat euismod nisl vel dapibus. Nam vitae ligula ut metus interdum facilisis ut ac massa. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-
-          Nulla erat mauris, hendrerit quis commodo vitae, dapibus mattis odio. In metus massa, scelerisque vel libero at, sollicitudin aliquet tellus. Nullam vitae pretium dui. Donec mauris arcu, venenatis quis eleifend sed, pellentesque id turpis. Curabitur eu nunc eget quam pharetra volutpat eu quis diam. Vestibulum ullamcorper suscipit enim sit amet cursus. Sed aliquet sagittis est. Vestibulum et sem quis est sagittis rutrum. Phasellus ut maximus erat. Cras mi ex, mattis ac porttitor quis, hendrerit nec est. Integer in nunc eu velit fringilla rutrum.
-
-          Praesent eget diam eu orci pulvinar dapibus. Suspendisse consequat, urna sed tristique pharetra, libero leo laoreet quam, eu tincidunt urna turpis ut tortor. Vivamus quis lectus lacinia, commodo risus vitae, pretium nibh. Mauris sed justo nec erat dignissim tincidunt faucibus et lacus. Vestibulum et rhoncus elit. Nunc vel eros rutrum, mattis nibh a, imperdiet justo. Nulla accumsan varius congue. Fusce egestas ex vitae ipsum commodo, sit amet ultrices odio suscipit.
-
-          Etiam ullamcorper ex vitae ante fringilla, eu feugiat dolor finibus. Fusce nec tempus eros, eu sagittis lectus. In condimentum, purus at commodo condimentum, massa augue semper mi, commodo vehicula odio lorem at nisl. Vivamus eget imperdiet magna. Duis sit amet tortor dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin ante mi, sollicitudin faucibus facilisis ac, maximus vitae arcu. Vivamus efficitur quam sed arcu auctor, ut venenatis ex ultricies. Curabitur in laoreet nisl. Donec a dui convallis ipsum maximus tincidunt vel ut augue. Praesent tellus neque, pharetra eu elit ut, rutrum pulvinar neque. Duis eu est nibh. Nulla hendr
-
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec nibh a velit ultricies finibus id ut risus. Aenean at fermentum enim, at efficitur arcu. Mauris ultricies massa ut quam pulvinar elementum. Nunc ut dolor in mauris egestas dictum. Aenean nisl ante, mattis non metus sodales, tempor pretium ante. Fusce congue semper lorem vel gravida. Aliquam laoreet sapien vitae sem aliquam, suscipit bibendum mi venenatis. Ut eu elementum purus, non convallis lacus. Mauris maximus sed dolor in convallis. Vestibulum at nisi odio. Nulla vel ligula consectetur, dictum leo at, fringilla risus. Aliquam dui enim, vehicula volutpat dictum eget, eleifend a libero. In accumsan purus vel euismod efficitur. Morbi placerat euismod nisl vel dapibus. Nam vitae ligula ut metus interdum facilisis ut ac massa. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-
-          Nulla erat mauris, hendrerit quis commodo vitae, dapibus mattis odio. In metus massa, scelerisque vel libero at, sollicitudin aliquet tellus. Nullam vitae pretium dui. Donec mauris arcu, venenatis quis eleifend sed, pellentesque id turpis. Curabitur eu nunc eget quam pharetra volutpat eu quis diam. Vestibulum ullamcorper suscipit enim sit amet cursus. Sed aliquet sagittis est. Vestibulum et sem quis est sagittis rutrum. Phasellus ut maximus erat. Cras mi ex, mattis ac porttitor quis, hendrerit nec est. Integer in nunc eu velit fringilla rutrum.
-
-          Praesent eget diam eu orci pulvinar dapibus. Suspendisse consequat, urna sed tristique pharetra, libero leo laoreet quam, eu tincidunt urna turpis ut tortor. Vivamus quis lectus lacinia, commodo risus vitae, pretium nibh. Mauris sed justo nec erat dignissim tincidunt faucibus et lacus. Vestibulum et rhoncus elit. Nunc vel eros rutrum, mattis nibh a, imperdiet justo. Nulla accumsan varius congue. Fusce egestas ex vitae ipsum commodo, sit amet ultrices odio suscipit.
-
-          Etiam ullamcorper ex vitae ante fringilla, eu feugiat dolor finibus. Fusce nec tempus eros, eu sagittis lectus. In condimentum, purus at commodo condimentum, massa augue semper mi, commodo vehicula odio lorem at nisl. Vivamus eget imperdiet magna. Duis sit amet tortor dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin ante mi, sollicitudin faucibus facilisis ac, maximus vitae arcu. Vivamus efficitur quam sed arcu auctor, ut venenatis ex ultricies. Curabitur in laoreet nisl. Donec a dui convallis ipsum maximus tincidunt vel ut augue. Praesent tellus neque, pharetra eu elit ut, rutrum pulvinar neque. Duis eu est nibh. Nulla hendr
+          <div className="charCreatorForm__baseInfo">
+            base info here
+          </div>
+          <div className="charCreatorForm__graphice">
+            graphice
+          </div>
         </div>
-      </div>ż
+      </div>
     </main>
   );
 };
