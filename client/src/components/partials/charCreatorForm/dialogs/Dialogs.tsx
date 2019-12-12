f2fd4c8ce0a0fbb3fb8449206cef8bd9c066c868
Rafał Kostecki <edward-12@o2.pl>
2019-12-12 18:50:26 +0100

    Add translations to Dialogs component

--- client/src/components/partials/charCreatorForm/dialogs/Dialogs.tsx
+++ client/src/components/partials/charCreatorForm/dialogs/Dialogs.tsx
@@ -13,6 +13,9 @@ import {
   findConnectedDialog 
 } from '../../../../assets/scripts/dialogs/findConnectedDialog';
 
+//Import contexts
+import { ContentContext } from '../../../../Template';
+
 
 interface IDialogs {
   type: string,
@@ -33,54 +36,58 @@ const Dialogs: React.FC<IDialogs> = ({ type, addBtnText }) => {
   }
 
   return (
-    <div className="dialogs">
-      <nav className="dialogs__nav">
-        <ul>
-          <li className="t-paragraph8Light"> { type } </li>
-          <li className="t-paragraph5Normal">
-            <span> { addBtnText } </span>
-          </li>
-        </ul>
-      </nav>
-      {
-        type === 'dialogs' ? (
-          exampleDialogs.length > 0 ? (
-            exampleDialogs.map((dialog, index) => {
-              return <Dialog 
-                id={dialog.id}
-                npc={dialog.npc}
-                player={dialog.player}
-                key={index}
-                validatorFunc={dialogsValidator}
-                connectedDialogs={connectedDialogs}
-                clearValidator={clearConnected}
-              />
-            })
-          ) : (
-            <p className='dialogs--none t-paragraph5Normal'>
-              There's no { type }
-            </p>
-          )
-        ) : null
-      }
-      {
-        type ==='monologs' ? (
-          exampleMonologs.length > 0 ? (
-            exampleMonologs.map((monolog, index) => {
-              return <Monolog 
-                id={monolog.id}
-                content={monolog.content}
-                key={index}
-              />
-            })
-          ) : (
-            <p className='dialogs--none t-paragraph5Normal'>
-              There's no { type }
-            </p>
-          )
-        ) : null
-      }
-    </div>
+    <ContentContext.Consumer>
+			{({ char }) => (
+        <div className="dialogs">
+          <nav className="dialogs__nav">
+            <ul>
+              <li className="t-paragraph8Light"> { type } </li>
+              <li className="t-paragraph5Normal">
+                <span> { addBtnText } </span>
+              </li>
+            </ul>
+          </nav>
+          {
+            type === char.form.dialogs.title ? (
+              exampleDialogs.length > 0 ? (
+                exampleDialogs.map((dialog, index) => {
+                  return <Dialog 
+                    id={dialog.id}
+                    npc={dialog.npc}
+                    player={dialog.player}
+                    key={index}
+                    validatorFunc={dialogsValidator}
+                    connectedDialogs={connectedDialogs}
+                    clearValidator={clearConnected}
+                  />
+                })
+              ) : (
+                <p className='dialogs--none t-paragraph5Normal'>
+                  There's no { type }
+                </p>
+              )
+            ) : null
+          }
+          {
+            type === char.form.monologs.title ? (
+              exampleMonologs.length > 0 ? (
+                exampleMonologs.map((monolog, index) => {
+                  return <Monolog 
+                    id={monolog.id}
+                    content={monolog.content}
+                    key={index}
+                  />
+                })
+              ) : (
+                <p className='dialogs--none t-paragraph5Normal'>
+                  There's no { type }
+                </p>
+              )
+            ) : null
+          }
+        </div>
+      )}
+    </ContentContext.Consumer>
   )
 }
 
