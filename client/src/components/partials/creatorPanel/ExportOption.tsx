6cc64c6d1d25ad2c4b7d7d427480a0e25c3badb9
Rafał Kostecki <edward-12@o2.pl>
2019-12-18 18:46:04 +0100

    Translate ExportOption

--- client/src/components/partials/creatorPanel/ExportOption.tsx
+++ client/src/components/partials/creatorPanel/ExportOption.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 
+//Import contexts
+import { ContentContext } from '../../../Template';
+
 
 const ExportOption: React.FC = () => {
 
   return (
-    <div 
-      role="button" 
-      className="option option--textOption option--smallerMargin" 
-      data-title="export your board to Angotia"
-    >
-      <span>Export</span>
-    </div>
+    <ContentContext.Consumer>
+			{({ creator }) => (
+        <div 
+          role="button" 
+          className="option option--textOption option--smallerMargin" 
+          data-title={creator.panel.options.export.dataTitle}
+        >
+          <span>
+            {creator.panel.options.export.content}
+          </span>
+        </div>
+      )}
+    </ContentContext.Consumer>
   );
 };
 
