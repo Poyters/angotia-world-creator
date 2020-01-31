5210bc06778361ca7c239d2cb03668e3b92d0d6f
Rafał Kostecki <edward-12@o2.pl>
2020-01-31 19:11:40 +0100

    AMC-96 add translations to Help

--- client/src/components/views/Help.tsx
+++ client/src/components/views/Help.tsx
@@ -38,61 +38,39 @@ const Help: React.FC = () => {
         </h1>
         <div className="scrollWrapper">
           <section>
-            Okey! If you are here it means that you chose your map size. 
-            You needed to type size in “field” unit. Each field contains a 
-            four square units. It’s really good show by the net option. 
-            Fields are marked by solid gray line, squares are marked by 
-            cropped gray line. You can manipulate the nets view by Net 
-            Option <div className="iconWrapper">
+            { help.field } <div className="iconWrapper">
               <NetOption viewTypeQuantity={3} />
-            </div> (more info about it at the Creator Panel features). 
+            </div> { help.field1 }
           </section>
 
           <section>
-            Another important thing to know about creating maps is a Select 
-            Option. You can choose different select type, by field, by square, 
-            or by mouse movement. You want to make field/square impossible to
-            move by any chars? Just block it. Use Block Option 
+            { help.selectBlock }
             <div className="iconWrapper iconWrapper--down">
               <BlockOption />
-            </div>. You can unblock by select fields/squares, press ctrl button 
-            and then click on Block Option.
+            </div>{ help.selectBlock1 }
           </section>
 
           <section>
-            To set background image, you need to click on 
+            { help.bg }
             <div className="iconWrapper iconWrapper--down"> 
               <AddFileOption />
-            </div>. 
+            </div>{ help.bg1 }
           </section>
 
           <section>
-            You need to know that important things to build maps are images. 
-            You can find them in Files Panel (FS). FS has bookmarks which 
-            contains needed images. Remember that bookmarks are a groups of 
-            images. Each group is above or below other.
+            { help.fs }
           </section>
 
           <section>
-            You can import your own image by Import Option image here>. 
-            Your imported images will be only exists on your Poyters account.
-            If you export complete map to Angotia game and it will be accepted
-            then your images appear for all Map Creators as a base images.
+            { help.import }
           </section>
 
           <section>
-            Do you want to create different level of difficulty for subsoils?
-            For instance, you want to define sand subsoil and a lane. That cause
-            different character behaviors. More weight of vertex means more
-            difficulties. So proper behavior is set lane's weight at 1, sand
-            at 3, chills at 5 etc.
+            { help.vertex }
           </section>
 
           <section>
-            If you know all that stuff you can create maps, yet! Yeah, that’s
-            so easy. Now you can select needed fields/squares and do some
-            actions on them, like - block squares, set images etc. So, enjoy,
-            the new AMC User!
+            { help.end }
           </section>
         </div>
       </article>
