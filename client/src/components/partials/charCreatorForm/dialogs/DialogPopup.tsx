a90a426db64c4707311f9d345824996ba9f4efaa
Rafał Kostecki <edward-12@o2.pl>
2020-01-31 17:22:55 +0100

    AMC-96 add translations to DialogPopup

--- client/src/components/partials/charCreatorForm/dialogs/DialogPopup.tsx
+++ client/src/components/partials/charCreatorForm/dialogs/DialogPopup.tsx
@@ -12,6 +12,9 @@ import { changeDialogs, changeTemponaryPlayerDialogs } from '../../../../redux/a
 //Import interfaces
 import { IDialog, IPlayer } from '../../../../assets/interfaces/dialogsInterfaces';
 
+//Import contexts
+import { ContentContext } from '../../../../Template';
+
 
 interface IDialogPopup {
   togglePopup: Function
@@ -68,66 +71,72 @@ const DialogPopup: React.FC<IDialogPopup> = ({ togglePopup }) => {
   };
 
   return (
-    <div className="g-container g-container--popup">
-      <div role="alert" className="insertPopup insertPopup--dialog"> 
-        <div 
-          className="g-exitBtn g-exitBtn--popup"
-          onClick={():void => togglePopup(false)}
-        > </div>
-        <header className="insertPopup__header t-paragraph3Light">
-          Add new dialog
-        </header>
-        <CharInputField
-          label='ID - auto generated'
-          inputValue={dialogId}
-          inputDisabled={true}
-        />
-        <label className="insertPopup__label t-paragraph6Light">
-          Npc dialog
-        </label>
-        <textarea
-          value={npcText} 
-          onChange={e => setNpcText(e.target.value)}
-        />
-        {
-          (npcTextErr) ? (
-            <span className="insertPopup--error">You need to type npc dialog</span>
-          ) : null
-        }
-        <nav className="playerDialogsHeader">
-          <header 
-            className="playerDialogsHeader__title t-paragraph5Light"
-      
-          >
-            Player dialogs
-          </header>
-          <div 
-            className="playerDialogsHeader__add t-paragraph5Normal"
-            onClick={():void => addPlayerDialogHandler() }
-          >
-            New Player dialog
+    <ContentContext.Consumer>
+			{({ char }) => (
+        <div className="g-container g-container--popup">
+          <div role="alert" className="insertPopup insertPopup--dialog"> 
+            <div 
+              className="g-exitBtn g-exitBtn--popup"
+              onClick={():void => togglePopup(false)}
+            > </div>
+            <header className="insertPopup__header t-paragraph3Light">
+              { char.dialogPopup.add }
+            </header>
+            <CharInputField
+              label='ID - auto generated'
+              inputValue={dialogId}
+              inputDisabled={true}
+            />
+            <label className="insertPopup__label t-paragraph6Light">
+              { char.dialogPopup.npcDialog }
+            </label>
+            <textarea
+              value={npcText} 
+              onChange={e => setNpcText(e.target.value)}
+            />
+            {
+              (npcTextErr) ? (
+                <span className="insertPopup--error">
+                  { char.dialogPopup.npcTextErr }
+                </span>
+              ) : null
+            }
+            <nav className="playerDialogsHeader">
+              <header 
+                className="playerDialogsHeader__title t-paragraph5Light"
+          
+              >
+                { char.dialogPopup.playerDialogs }
+              </header>
+              <div 
+                className="playerDialogsHeader__add t-paragraph5Normal"
+                onClick={():void => addPlayerDialogHandler() }
+              >
+                { char.dialogPopup.newPlayerDialog }
+              </div>
+
+            </nav>
+            
+            <div className="playerDialogsWrapper">
+              { 
+                temponaryPlayerDialogs.map((playerDialog: IPlayer) => {
+                  return <PlayerDialog playerId={playerDialog.id} key={playerDialog.id} />;
+                })
+              }
+            </div>      
+
+            <button 
+              type="submit" 
+              className="insertPopup__submit t-paragraphLight" 
+              onClick={(): void => submitHandler()} 
+              disabled={npcTextErr}
+            > 
+              { char.dialogPopup.submit }
+            </button>
           </div>
-
-        </nav>
-        
-        <div className="playerDialogsWrapper">
-          { 
-            temponaryPlayerDialogs.map((playerDialog: IPlayer) => {
-              return <PlayerDialog playerId={playerDialog.id} key={playerDialog.id} />;
-            })
-          }
-        </div>      
-
-        <button 
-          type="submit" 
-          className="insertPopup__submit t-paragraphLight" 
-          onClick={(): void => submitHandler()} 
-          disabled={npcTextErr}
-        > 
-          submit 
-        </button>
-      </div>
-    </div>
+        </div>
+      )}
+    </ContentContext.Consumer>
   );
 };
 
