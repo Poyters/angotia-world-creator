c654bb35393df7bdb590ee074d34d51c50207165
Rafał Kostecki <edward-12@o2.pl>
2020-09-22 15:13:41 +0200

    AWC-193 add tests to prepareInternalCharData

--- src/assets/mocks/content.ts
+++
@@ -1,5 +0,0 @@
-import { enContent } from '../content/langs/en/index';
-import React from 'react';
-
-export const MockContent = React.createContext(enContent);
-export const content = enContent;
--- src/assets/scripts/parsers/prepareInternalCharData.ts
+++ src/assets/scripts/parsers/prepareInternalCharData.ts
@@ -3,7 +3,7 @@ import { deepCopy } from '../utils/deepCopy';
 const camelcaseKeys = require('camelcase-keys');
 
 
-export const prepareInternalCharData = (charData: ICharState) => {
+export const prepareInternalCharData = (charData: any): ICharState => {
   const dataCopy = deepCopy(charData);
   dataCopy.internalId = dataCopy._id;
   delete dataCopy._id;
