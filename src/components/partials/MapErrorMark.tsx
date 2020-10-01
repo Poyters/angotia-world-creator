be63a1eb4b927ae91c6582ce8c3da8f83e7c185d
Rafał Kostecki <edward-12@o2.pl>
2020-10-01 22:34:55 +0200

    AWC-202 connect error mark to error panel

--- src/components/partials/MapErrorMark.tsx
+++ src/components/partials/MapErrorMark.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { IStore } from '../../interfaces/store.interface';
+import { toggleErrorPanel } from '../../store/actions/uiActions';
 
 
 export const MapErrorMark: React.FC = () => {
+  const mapErrors: string[] = useSelector((state: IStore) => state.ui.mapCreationErrors);
+  const dispatch = useDispatch();
+
   return (
-    <aside className="mapErrorMark">
-      <div className="mapErrorMark__count"> 
-        <span> ! </span>
-      </div>
-      <div className="mapErrorMark__message"> 
-        <span> 17 errors </span>
-      </div>
-    </aside>
+    <>
+      {
+        mapErrors.length > 0 ? (
+          <aside 
+            className="mapErrorMark"
+            onClick={(): void => dispatch(toggleErrorPanel(true))}
+          >
+            <div className="mapErrorMark__count"> 
+              <span> ! </span>
+            </div>
+            <div className="mapErrorMark__message"> 
+              <span> { mapErrors.length } errors </span>
+            </div>
+          </aside>
+        ) : null
+      }
+    </>
   );
 };
