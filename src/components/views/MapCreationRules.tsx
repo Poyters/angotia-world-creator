4b8a0bcf605135047bf01cd25fa509c8f482ca78
Rafał Kostecki <edward-12@o2.pl>
2020-10-01 19:18:00 +0200

    AWC-202 add map creation rules

--- src/components/views/MapCreationRules.tsx
+++ src/components/views/MapCreationRules.tsx
@@ -31,7 +31,7 @@ export const MapCreationRules: React.FC = () => {
             <ol>
               <li>Max MOBs quantity - at map should not be a huge amount of mobs due to potential lags. So you can add maximum 1 mob per 8 fields. It means that board 8x8 (contains 64 fields) can has max 8 mobs. </li>
               <li>Max NPCs quantity - map should has not contain too much NPC. Try to keep as much actions as you can in exists NPC or a small number of them. Maxium 1 NPC per 5 fields</li>
-              <li>Max SEs quantity - maxium 1 NPC per 4 fieldss</li>
+              <li>Max SEs quantity - maxium 1 NPC per 4 fields</li>
               <li>Max passages quantity - maxium 1 passage field per 4 fields</li>
               <li>Max block squares quantity - maxium 1 block square per 0.5 field. It means, that 8x8 map, can has maximum 128 blocked quares</li>
               <li>Map name - each map should has name. It need to be longer than 5 letters, without any special chars</li>
