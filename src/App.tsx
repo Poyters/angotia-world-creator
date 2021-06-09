65038d7791e868d736cac760daa1db95c9b929ac
Rafał Kostecki <edward-12@o2.pl>
2021-06-09 14:49:20 +0200

    AWC-221 delete template component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,39 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Template } from './Template';
+import './assets/styles/index.scss';
 import { Home } from './components/views/Home';
+import { MapCreator } from './components/views/MapCreator';
+import { CharCreator } from './components/views/CharCreator';
+import { Help } from './components/views/Help';
+import { License } from './components/views/License';
+import { Features } from './components/views/Features';
+import { NotFound } from './components/views/NotFound';
+import { MapCreationRules } from './components/views/MapCreationRules';
+import { Notifications } from './components/partials/Notifications';
+import { LoadRequestedMap } from './components/views/LoadRequestedMap';
+import { LoadRequestedChar } from './components/views/LoadRequestedChar';
 
 
 export const App: React.FC = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/:lang?" component={Template}>
+    <>
+      <Notifications />
+      <Router>
+        <Switch>
           <Route exact path="/" component={Home} />
-        </Route>
-      </Switch>
-    </Router>
+          <Route path={'/map-creator'} component={MapCreator} />
+          <Route path={'char-creator'} component={CharCreator} />
+          <Route path={'help'} component={Help} />
+          <Route path={'license'} component={License} />
+          <Route path={'features'} component={Features} />
+          <Route 
+            path={'/map-creation-rules'}
+            component={MapCreationRules} />
+          <Route path={'/load-req-map'} component={LoadRequestedMap} />
+          <Route path={'/load-req-char'} component={LoadRequestedChar} />
+          <Route component={NotFound}/>
+        </Switch>
+      </Router>
+    </>
   );
 };
--- src/components/partials/ReportIssue.tsx
+++ src/components/partials/ReportIssue.tsx
@@ -1,17 +1,14 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import appConfig from '../../assets/configs/app.config.json';
-import { ContentContext } from '../../Template';
 
 
 export const ReportIssue: React.FC = () => {
-  const { global } = useContext(ContentContext);
-
   return (
     <aside 
       className="labelMark labelMark--bottomLeft labelMark--link t-paragraph5Normal"
     >
       <a href={appConfig?.reportIssue?.link}>
-        {global?.reportIssue?.name}
+        Report issue
       </a>
     </aside>
   );
