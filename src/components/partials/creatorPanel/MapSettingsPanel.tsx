1bbeb31a041c2d0094f2195f2768cb79053a7e27
Rafał Kostecki <edward-12@o2.pl>
2021-06-16 11:23:59 +0200

    AWC-221 translate map settings panel

--- src/components/partials/creatorPanel/MapSettingsPanel.tsx
+++ src/components/partials/creatorPanel/MapSettingsPanel.tsx
@@ -6,8 +6,8 @@ import { IStore } from '../../../interfaces/store.interface';
 import { useTranslation } from 'react-i18next';
 
 
-export const MapSettingsPanel: React.FC = () => {
-	const { t } = useTranslation(['common']);
+export const MapSettingsPanel = () => {
+	const { t } = useTranslation(['common', 'map']);
 	const [isOpen, setIsOpen] = useState<boolean>(false);
 	const description = useSelector((state: IStore) => state.map.description);
 	const [descValue, setDescValue] = useState<string>(description);
@@ -27,7 +27,7 @@ export const MapSettingsPanel: React.FC = () => {
 				className="g-sidePanelSwitch g-sidePanelSwitch--statisticPanel t-paragraph4Normal" 
 				onClick={() => setIsOpen(true)}
 			> 
-				{ 'creator?.settingsPanel?.open' }
+				{ t('map:settings.title') }
 			</div>
 			<aside 
 				className="g-sidePanelWrapper g-sidePanelWrapper--left" 
@@ -35,29 +35,29 @@ export const MapSettingsPanel: React.FC = () => {
 			>
 				<div className="g-sidePanel g-sidePanel--left">
 					<ActionInputField
-						label={'creator?.settingsPanel?.id'}
+						label={t('common:autoId')}
 						inputValue={mapInternalId}
 						inputDisabled={true}
 					/>
 					<ActionInputField
-						label={'creator?.settingsPanel?.size'}
+						label={t('map:settings.size')}
 						inputValue={`${mapSize.x} x ${mapSize.y}`}
 						inputDisabled={true}
 					/>
 					<ActionInputField
-						label={'creator?.settingsPanel?.boardName'}
+						label={t('map:settings.mapName')}
 						inputValue={boardName}
 						action={changeMapName}
 					/>
 					<ActionInputField
-						label={'creator?.settingsPanel?.minLevel'}
+						label={t('map:settings.minEntryLevel')}
 						inputValue={minEntryLevel}
 						action={setMinEntryLevel}
 					/>
 
 					<div className="g-separeTextarea">
 						<label className="g-separeTextarea__label t-paragraph6Light">
-							{ 'creator?.settingsPanel?.description' }
+							{ t('map:settings.description') }
 						</label>
 						<textarea
 							className="g-separeTextarea__area g-separeTextarea__area--mapDesc"
