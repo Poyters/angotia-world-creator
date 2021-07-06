70f8063c1702b644ef8fce68691ab601fadaa2ce
Rafał Kostecki <edward-12@o2.pl>
2021-07-06 16:58:37 +0200

    AWC-264 test user id

---
+++ src/scripts/user/createUserId.test.ts
@@ -0,0 +1,16 @@
+import { createUserId } from './createUserId';
+
+
+describe('createUserId script', () => {
+	it('is a function', () => {
+		expect(typeof createUserId).toBe('function');
+  });
+
+  it('createUserId 1', () => {
+		expect(createUserId().length).toBe(24);
+  });
+
+	it('createUserId 2', () => {
+		expect(createUserId().split('')[13]).toBe('_');
+  });
+});
