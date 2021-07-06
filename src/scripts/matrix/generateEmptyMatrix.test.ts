1890630f1565750c9def8f02940e9f60d9103407
Rafał Kostecki <edward-12@o2.pl>
2021-07-06 17:04:51 +0200

    AWC-264 test generate empty matrix

--- src/scripts/matrix/generateEmptyMatrix.test.ts
+++ src/scripts/matrix/generateEmptyMatrix.test.ts
@@ -12,8 +12,8 @@ describe('generateEmptyMatrix script', () => {
 
 	it('Check generate empty matrix', () => {
     expect(isEmptyMatrix(newEmptyMatrix)).toBe(true);
-    expect(newEmptyMatrix.length).toEqual(mapState.size.x);
-    expect(newEmptyMatrix[0].length).toEqual(mapState.size.y);
+    expect(newEmptyMatrix.length).toEqual(mapState.size.y);
+    expect(newEmptyMatrix[0].length).toEqual(mapState.size.x);
   });
   
   it('Check if contains empty fields', () => {
@@ -27,5 +27,33 @@ describe('generateEmptyMatrix script', () => {
     });
 
     expect(containValues).toBe(false);
+  });
+
+  it('generateEmptyMatrix 1', () => {
+    const new1 = generateEmptyMatrix({ x: 1, y: 3 });
+    expect(isEmptyMatrix(new1)).toBe(true);
+    expect(new1.length).toEqual(3);
+    expect(new1[0].length).toEqual(1);
+  });
+
+  it('generateEmptyMatrix 2', () => {
+    const new1 = generateEmptyMatrix({ x: 3, y: 6 });
+    expect(isEmptyMatrix(new1)).toBe(true);
+    expect(new1.length).toEqual(6);
+    expect(new1[0].length).toEqual(3);
+  });
+
+  it('generateEmptyMatrix 3', () => {
+    const new1 = generateEmptyMatrix({ x: 100, y: 3 });
+    expect(isEmptyMatrix(new1)).toBe(true);
+    expect(new1.length).toEqual(3);
+    expect(new1[0].length).toEqual(100);
+  });
+
+  it('generateEmptyMatrix 4', () => {
+    const new1 = generateEmptyMatrix({ x: 30, y: 12 });
+    expect(isEmptyMatrix(new1)).toBe(true);
+    expect(new1.length).toEqual(12);
+    expect(new1[0].length).toEqual(30);
   });
 });
