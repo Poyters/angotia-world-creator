0f36e99518018a076808215b0d080536731c3f68
Rafał Kostecki <edward-12@o2.pl>
2022-01-24 14:51:09 +0100

    AWC-308 fix apollo client types

--- src/api/angotiaResources/arApiInit.ts
+++ src/api/angotiaResources/arApiInit.ts
@@ -1,14 +1,8 @@
-import { ApolloClient } from "apollo-client";
-import { InMemoryCache, NormalizedCacheObject } from "apollo-cache-inmemory";
-import { HttpLink } from "apollo-link-http";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const link = new HttpLink({
-  uri: process.env.REACT_APP_ANGOTIA_RESOURCES_URL
-});
-
-export const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+export const client = new ApolloClient({
   cache: new InMemoryCache({
     addTypename: false
   }),
-  link
+  uri: process.env.REACT_APP_ANGOTIA_RESOURCES_URL
 });
